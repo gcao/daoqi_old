54edf817290d4211e4b68ce12157ae4b6b2ff17f
Guoliang Cao <gcao@gcaomac.local>
2008-09-14 08:57:08 -0400

    changes to configuration and layout etc

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 set :application, "daoqi"
-set :repository,  "git@heroku.com:daoqi.git"
+set :repository,  "git@github.com:gcao/daoqi.git"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/gcao/deployment/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
