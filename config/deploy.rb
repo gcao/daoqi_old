80272ff25ccc4a0686ec94a3d99c6a7282b46632
Guoliang Cao <gcao@gcaomac.local>
2008-09-14 09:16:56 -0400

    changed deploy.rb to restart mod_rails instead of using the default method

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,24 @@
+my_server = "209.20.88.80"
 set :application, "daoqi"
 set :repository,  "git@github.com:gcao/daoqi.git"
 set :runner, "gcao"
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
 set :deploy_to, "/home/gcao/deployment/#{application}"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
 set :scm, :git
-
-my_server = "209.20.88.80"
+set :deploy_via, :remote_cache
 
 role :app, my_server
 role :web, my_server
 role :db,  my_server, :primary => true
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
